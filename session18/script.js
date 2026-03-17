265807254afd2fdd538fbc41596ed7cdea8d87d8
vudungdeptrai2007-glitch <vudungdeptrai.2007@gmail.com>
2026-03-18 06:47:59 +0700

    Add files via upload

---
+++ session18/script.js
@@ -0,0 +1,19 @@
+let products = []
+
+let productName = document .getelementById('productName');
+let productcategory = document .getelementById('productcategory');
+let productPrice = document .getelementById('productPrice');
+let productQuantity = document .getelementById('productQuantity');
+let productDescription = document .getelementById('productDescription');
+
+let productForm = document.getelementById('productForm')
+
+
+
+
+function createProduct (){
+
+    let newProduct = {
+        id 
+    }
+}
